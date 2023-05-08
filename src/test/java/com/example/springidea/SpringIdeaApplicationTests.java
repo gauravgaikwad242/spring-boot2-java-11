baff93ffae331386d9c3c6a9890e8693d2c8239a
gauravgaikwad242 <gaikwadg576@gmail.com>
2023-05-08 16:29:35 +0530

    init

---
+++ src/test/java/com/example/springidea/SpringIdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springidea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIdeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
