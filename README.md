a93ce161b2ab5d14171fa374fc030e8e68f51bf7
gauravgaikwad242 <gaikwadg576@gmail.com>
2023-05-09 10:17:49 +0530

    add command

---
+++ README.md
@@ -0,0 +1,1 @@
+current base branch
--- src/main/java/com/example/springidea/controller/PublicRestController.java
+++ src/main/java/com/example/springidea/controller/PublicRestController.java
@@ -1,12 +1,22 @@
 package com.example.springidea.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/home")
 public class PublicRestController {
     @GetMapping
     public String handler(){
         return "app running!!";
+    }
+    @GetMapping("/all")
+    public String getAll(){
+        return "app all running!!";
+    }
+    @GetMapping("/root")
+    public String root(){
+        return "app root running!!";
     }
 }
